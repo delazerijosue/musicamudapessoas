7d05413a5f2578f30545a67a5702b6537d00901d
Josué Delazeri <josue.delazeri@gmail.com>
2016-12-04 23:38:59 -0200

    Add files via upload

---
+++ sketch3.js
@@ -0,0 +1,36 @@
+var marina_01, marina_02, marina_03, marina_04, marina_05;
+var amp;
+var song, analyzer;
+
+function preload() {
+  // load images and sound
+
+  song = loadSound('ooh-la.mp3');
+
+  marina_01 = loadImage("marina_01.jpg");
+  marina_02 = loadImage("marina_02.jpg");
+  marina_03 = loadImage("marina_03.jpg");
+  marina_04 = loadImage("marina_04.jpg");
+  marina_05 = loadImage("marina_05.jpg");
+}
+
+function setup() {
+  song.play();
+  createCanvas(720, 380);
+
+  //song = loadSound("ooh-la.mp3", loaded);
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(song);
+}
+
+function draw() {
+  background(25);
+
+  var rms = analyzer.getLevel();
+
+  image(marina_01, 1+rms*20, 0);
+  image(marina_02, 1-rms*30, 76);
+  image(marina_03, 1+rms*20, 152);
+  image(marina_04, 1-rms*40, 228);
+  image(marina_05, 1+rms*15, 304);
+}
