04e534fcc6d6fba4450d39f6e10ca8138c805cd3
Josué Delazeri <josue.delazeri@gmail.com>
2016-12-07 17:35:02 -0200

    Update sketch3.js

--- sketch3.js
+++ sketch3.js
@@ -8,11 +8,11 @@ function preload() {
   song = loadSound('ooh-la.mp3');
 
   azul_20 = loadImage("azul_20.png");
-  marina_01 = loadImage("marina_01.jpg");
-  marina_02 = loadImage("marina_02.jpg");
-  marina_03 = loadImage("marina_03.jpg");
-  marina_04 = loadImage("marina_04.jpg");
-  marina_05 = loadImage("marina_05.jpg");
+  marina_01 = loadImage("marina_01.png");
+  marina_02 = loadImage("marina_02.png");
+  marina_03 = loadImage("marina_03.png");
+  marina_04 = loadImage("marina_04.png");
+  marina_05 = loadImage("marina_05.png");
 }
 
 function setup() {
